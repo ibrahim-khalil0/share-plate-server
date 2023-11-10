8d6919a32ba617b5ffa81d81f47375e2301ef783
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2023-11-10 10:39:02 +0600

    get single food from database done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -64,7 +64,13 @@ app.get('/foods', async(req, res) => {
 
 
 
-
+// get single food 
+app.get('/food/:id', async(req, res) => {
+  const id = req.params.id
+  const query = {_id : new ObjectId(id)}
+  const food = await foodsCollection.findOne(query)
+  res.send(food)
+})
 
 
 
